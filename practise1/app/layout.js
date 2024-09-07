df0a5f7ca3101badc95b7b30272ced9ff74afc7c
Your Name <your.email@example.com>
2024-09-07 11:19:47 +0500

    myyyy

--- practise1/app/layout.js
+++ practise1/app/layout.js
@@ -21,6 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <h2>ITS MY PAGE OK </h2>
         {children}
       </body>
     </html>
