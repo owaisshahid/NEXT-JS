ac9f0c4b5964df17eb995a991064d0764d8859a7
Your Name <your.email@example.com>
2024-09-16 17:29:20 +0500

    ECOMM

--- ecomm/app/layout.js
+++ ecomm/app/layout.js
@@ -8,7 +8,6 @@ export default function RootLayout({ children }) {
     <html>
       <body>
      
-      <Navbar/>
       
         <main>{children}</main> {/* This will render your page content */}
         {/* <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz4fnFO9n8p8dff0oUbOpwI0siN7FqB6iAE9S8PpUnF7e0CeaT6umJlg5B" crossorigin="anonymous"></script>
